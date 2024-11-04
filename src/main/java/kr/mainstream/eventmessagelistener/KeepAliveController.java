49da953d78513fe41dc4c43da626bf7325140dc3
gimjinseong <wlsdiqkdrk@naver.com>
2024-11-04 23:09:22 +0900

    refactoring : 헬스 체크 API - 시간 로그 수정

--- src/main/java/kr/mainstream/eventmessagelistener/KeepAliveController.java
+++ src/main/java/kr/mainstream/eventmessagelistener/KeepAliveController.java
@@ -4,12 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 @Slf4j
 public class KeepAliveController {
     @GetMapping("/health")
     public String ping() {
         log.info("health check");
+        log.info("now : {}", LocalDateTime.now());
         return "ok";
     }
 }
